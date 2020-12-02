6ff7db2c8a93e362a000877f6e0a74aff01b0983
graphee-gabriel <contact.gabriel.morin@gmail.com>
2020-12-02 19:49:31 +0100

    clean gifs

--- constants/gifs.js
+++ constants/gifs.js
@@ -1,49 +1,67 @@
 export const GIFS = [
   "https://i.giphy.com/media/l3q2IT7TSEs01c0SY/giphy.mp4",
+
+  // tame impala
   "https://media4.giphy.com/media/l3fzOCIuFxXpmHe2A/giphy.mp4",
-  "https://media4.giphy.com/media/xTiTnumdcQABBLgM92/giphy.mp4",
+
+  // midnight gospel
   "https://media0.giphy.com/media/fVi9toGlv0tbMm1yoF/giphy.mp4",
   "https://media2.giphy.com/media/MBlSpxW9dqsiV8Vg4y/giphy.mp4",
   "https://media3.giphy.com/media/KeiN7PfEh6MjDWvLCH/giphy.mp4",
   "https://media3.giphy.com/media/dZKdgElhMNTiLcIHvZ/giphy.mp4",
   "https://media3.giphy.com/media/UV47YkTJbUFxSVnEuV/giphy.mp4",
-  "https://media3.giphy.com/media/3o7aD9hC4HyZ1ZPJgk/giphy.mp4",
-  "https://media3.giphy.com/media/pJEfYqdWXVjAk/giphy.mp4",
-  "https://media3.giphy.com/media/l0IsIJE63zBQBVwfm/giphy.mp4",
-  "https://media3.giphy.com/media/3o6Ztp7GlqlADQWWgE/giphy.mp4",
-  "https://media3.giphy.com/media/3o6j8fXbqweKIIfCJW/giphy.mp4",
-  "https://media3.giphy.com/media/13kmsD1jzeE0TK/giphy.mp4",
-  "https://media3.giphy.com/media/xN9GPT5JBw4es/giphy.mp4",
-  "https://media3.giphy.com/media/xT0GqcCJJJH12hJvGM/giphy.mp4",
-  "https://media3.giphy.com/media/l0HlUZNyQ8jHgYedy/giphy.mp4",
-  "https://media3.giphy.com/media/3BRDkVjKikYW4/giphy.mp4",
-  "https://media3.giphy.com/media/3o8doT9BL7dgtolp7O/giphy.mp4",
-  "https://media3.giphy.com/media/26tPplGWjN0xLybiU/giphy.mp4",
-  "https://media3.giphy.com/media/ASd0Ukj0y3qMM/giphy.mp4",
+  "https://media3.giphy.com/media/TGcbPlBYijiVLfQ46e/giphy.mp4",
+
+  // adventure times
   "https://media3.giphy.com/media/11pH16cX3QfJh6/giphy.mp4",
   "https://media3.giphy.com/media/hPs1oJT3WbJXYTCmwn/giphy.mp4",
   "https://media3.giphy.com/media/cgW5iwX0e37qg/giphy.mp4",
-  "https://media3.giphy.com/media/TGcbPlBYijiVLfQ46e/giphy.mp4",
   "https://media3.giphy.com/media/Z9iEijBJuufOPFiUCT/giphy.mp4",
   "https://media3.giphy.com/media/QWABFbl02oUt10UP9z/giphy.mp4",
-  "https://media3.giphy.com/media/L2UdIWuCRbUL6/giphy.mp4",
   "https://media3.giphy.com/media/f1fpMxNfg8GQw/giphy.mp4",
+  // "https://media3.giphy.com/media/L2UdIWuCRbUL6/giphy.mp4",
+
+  // bo jack
+  "https://media3.giphy.com/media/3o7aD9hC4HyZ1ZPJgk/giphy.mp4",
+  "https://media3.giphy.com/media/pJEfYqdWXVjAk/giphy.mp4",
+  "https://media3.giphy.com/media/3o6Ztp7GlqlADQWWgE/giphy.mp4",
+  "https://media3.giphy.com/media/3o6j8fXbqweKIIfCJW/giphy.mp4",
+  // "https://media3.giphy.com/media/l0IsIJE63zBQBVwfm/giphy.mp4",
+
+  // laputa
+  "https://media3.giphy.com/media/13kmsD1jzeE0TK/giphy.mp4",
+  "https://media3.giphy.com/media/xN9GPT5JBw4es/giphy.mp4",
+
+  // Stefanie Shank
+  "https://media3.giphy.com/media/xT0GqcCJJJH12hJvGM/giphy.mp4",
+  "https://media3.giphy.com/media/l0HlUZNyQ8jHgYedy/giphy.mp4",
+
+  // simpsons
+  "https://media3.giphy.com/media/3BRDkVjKikYW4/giphy.mp4",
+  "https://media3.giphy.com/media/3o8doT9BL7dgtolp7O/giphy.mp4",
+  "https://media3.giphy.com/media/26tPplGWjN0xLybiU/giphy.mp4",
+  // "https://media3.giphy.com/media/ASd0Ukj0y3qMM/giphy.mp4",
+  
   // rick and morty
   "https://media3.giphy.com/media/3oEduU2JjgQUGKyNvG/giphy.mp4",
+
   // beavis buthead
   "https://media3.giphy.com/media/dM2xuxnJCg4H6/giphy.mp4",
+
   // micah buzan
   "https://media3.giphy.com/media/5vURBXjZebEdgBuY0A/giphy.mp4",
   "https://media3.giphy.com/media/3o6Zti8dka85rQdrAA/giphy.mp4",
-  "https://media3.giphy.com/media/14fekZCgH9scLu/giphy.mp4",
   "https://media3.giphy.com/media/1xjX7Hfpb5DfNgIA4h/giphy.mp4",
-  "https://media3.giphy.com/media/BgoAg6dvBqV0Y/giphy.mp4",
-  "https://media3.giphy.com/media/xUOxfpgTBIQDWaKAw0/giphy.mp4",
   "https://media3.giphy.com/media/Sx8KbDQWc3Vlu/giphy.mp4",
   "https://media3.giphy.com/media/P4Svk2blROW0U/giphy.mp4",
   "https://media3.giphy.com/media/1j0QB5l4wZYST4v6/giphy.mp4",
   "https://media3.giphy.com/media/13Mph7VHug3NZe/giphy.mp4",
+  // "https://media3.giphy.com/media/14fekZCgH9scLu/giphy.mp4",
+  // "https://media3.giphy.com/media/BgoAg6dvBqV0Y/giphy.mp4",
+  // "https://media3.giphy.com/media/xUOxfpgTBIQDWaKAw0/giphy.mp4",
+
   // ori toor
+  "https://media4.giphy.com/media/xTiTnumdcQABBLgM92/giphy.mp4",
   "https://media3.giphy.com/media/d2YWUyUafZNpicSs/giphy.mp4",
   "https://media3.giphy.com/media/xT0BKK3deWS2rVicYE/giphy.mp4",
   "https://media3.giphy.com/media/3og0INRvt0D3hERC1O/giphy.mp4",
@@ -51,6 +69,7 @@ export const GIFS = [
   "https://media3.giphy.com/media/l0HlROu26C47rXWla/giphy.mp4",
   "https://media3.giphy.com/media/26FPxkG6aDBq4YK9W/giphy.mp4",
   "https://media3.giphy.com/media/ToMjGpNrjhYRbgZKAZa/giphy.mp4",
+
   // gasta
   "https://media3.giphy.com/media/3ohs8247Z6dNcTS7Zu/giphy.mp4",
   "https://media3.giphy.com/media/l1KdcgMmy0Rtkm0Ew/giphy.mp4",
