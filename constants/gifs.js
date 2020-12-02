91afe37d13b3f4f2788891f60ad7018b5607ce05
graphee-gabriel <contact.gabriel.morin@gmail.com>
2020-12-02 18:08:44 +0100

    new gifs more psychedelic

--- constants/gifs.js
+++ constants/gifs.js
@@ -26,4 +26,52 @@ export const GIFS = [
   "https://media3.giphy.com/media/TGcbPlBYijiVLfQ46e/giphy.mp4",
   "https://media3.giphy.com/media/Z9iEijBJuufOPFiUCT/giphy.mp4",
   "https://media3.giphy.com/media/QWABFbl02oUt10UP9z/giphy.mp4",
+  "https://media3.giphy.com/media/L2UdIWuCRbUL6/giphy.mp4",
+  "https://media3.giphy.com/media/f1fpMxNfg8GQw/giphy.mp4",
+  // rick and morty
+  "https://media3.giphy.com/media/3oEduU2JjgQUGKyNvG/giphy.mp4",
+  // beavis buthead
+  "https://media3.giphy.com/media/dM2xuxnJCg4H6/giphy.mp4",
+  // micah buzan
+  "https://media3.giphy.com/media/5vURBXjZebEdgBuY0A/giphy.mp4",
+  "https://media3.giphy.com/media/3o6Zti8dka85rQdrAA/giphy.mp4",
+  "https://media3.giphy.com/media/14fekZCgH9scLu/giphy.mp4",
+  "https://media3.giphy.com/media/1xjX7Hfpb5DfNgIA4h/giphy.mp4",
+  "https://media3.giphy.com/media/BgoAg6dvBqV0Y/giphy.mp4",
+  "https://media3.giphy.com/media/xUOxfpgTBIQDWaKAw0/giphy.mp4",
+  "https://media3.giphy.com/media/Sx8KbDQWc3Vlu/giphy.mp4",
+  "https://media3.giphy.com/media/P4Svk2blROW0U/giphy.mp4",
+  "https://media3.giphy.com/media/1j0QB5l4wZYST4v6/giphy.mp4",
+  "https://media3.giphy.com/media/13Mph7VHug3NZe/giphy.mp4",
+  // ori toor
+  "https://media3.giphy.com/media/d2YWUyUafZNpicSs/giphy.mp4",
+  "https://media3.giphy.com/media/xT0BKK3deWS2rVicYE/giphy.mp4",
+  "https://media3.giphy.com/media/3og0INRvt0D3hERC1O/giphy.mp4",
+  "https://media3.giphy.com/media/l2Sq0UCtYe9IoSs8g/giphy.mp4",
+  "https://media3.giphy.com/media/l0HlROu26C47rXWla/giphy.mp4",
+  "https://media3.giphy.com/media/26FPxkG6aDBq4YK9W/giphy.mp4",
+  "https://media3.giphy.com/media/ToMjGpNrjhYRbgZKAZa/giphy.mp4",
+  // gasta
+  "https://media3.giphy.com/media/3ohs8247Z6dNcTS7Zu/giphy.mp4",
+  "https://media3.giphy.com/media/l1KdcgMmy0Rtkm0Ew/giphy.mp4",
+  "https://media3.giphy.com/media/xUOxeZZc8QNmVQfzjO/giphy.mp4",
+  // gasta 2
+  "https://media3.giphy.com/media/3oEjHKTtnLAIlB2zsc/giphy.mp4",
+  "https://media3.giphy.com/media/l41YlRe9x0AxJAInK/giphy.mp4",
+  "https://media3.giphy.com/media/3oD3YC5okylCndTsT6/giphy.mp4",
+  "https://media3.giphy.com/media/l3UcEQRPDcMAS2Omk/giphy.mp4",
+  "https://media3.giphy.com/media/xT8qBguGO86OcnDnTq/giphy.mp4",
+  // foo fighters
+  "https://media3.giphy.com/media/3ohhwDBA9ieFU9ATqE/giphy.mp4",
+  // sah looter
+  "https://media3.giphy.com/media/xT1XGzJmDpDMwzSLhC/giphy.mp4",
+  "https://media3.giphy.com/media/3ov9k9NRJgG4rKA0vu/giphy.mp4",
+  "https://media3.giphy.com/media/xUPGczYETHpMVwT8C4/giphy.mp4",
+  "https://media3.giphy.com/media/3oz8xL64yRHAHIIXDy/giphy.mp4",
+  // carl burton
+  "https://media3.giphy.com/media/l4FGzpchvKed6VhKw/giphy.mp4",
+  "https://media3.giphy.com/media/26xBLcftQCC9P0WHu/giphy.mp4",
+  "https://media3.giphy.com/media/xT9IgoB9qTlMeDZg64/giphy.mp4",
+  // amanda bonaiuto
+  "https://media3.giphy.com/media/LQob9FD8ZMfsMtFZhC/giphy.mp4",
 ]
